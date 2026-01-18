309c13de5779fb0e49050c99bd60d0491494a183
Ayush Musale <aayushmusale05@gmail.com>
2026-01-18 19:04:06 +0530

    added Login

---
+++ backend/handlers/loginuser.js
@@ -0,0 +1,79 @@
+const pool = require('../DBconnection')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+async function loginUser(req, res) {
+    try{
+        let {email, password} = req.body;
+        
+        if(!email || !password){
+            return res.status(400).json({
+                status: 400,
+                message: "fields-requuired"
+            })
+        }
+
+        email = email.toLowerCase();
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+        if(!emailRegex.test(email)){
+            return res.status(400).json({
+                status: 400,
+                message: "invalid-email"
+            }) 
+        }
+
+        const [rows] = await pool.execute(
+            "SELECT id,email, password_hash, user_role FROM users WHERE email=?",
+            [email]
+        )
+
+        if(rows.length == 0){
+            return res.status(400).json({
+                status: 400,
+                message: "invalid-credentials"
+            })
+        }
+
+        const user = rows[0];
+        const isMatch =  await bcrypt.compare(password, user.password_hash);
+
+        if(!isMatch){
+            return res.status(401).json({
+                status: 401,
+                message: 'invalid-credentails',
+            })
+        }
+
+        const token =  jwt.sign(
+            {
+                'id': user.id,
+                'email': email,
+                'role': user.user_role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: '7d'
+            }
+        );
+
+        return res.status(200).json({
+                status: 201,
+                message: 'successful-login',
+                'token': token,
+                'user':{
+                    email: email,
+                    id: user.id,
+                }
+            })
+
+    }
+    catch(e){
+        return res.status(500).json({
+            status: 500,
+            message: "login-failed",
+        })
+    }
+}
+
+module.exports = {loginUser}
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const reg_router =  require("./routes/registration")
+const login_router = require('./routes/login')
+const {jwtAuth} = require('./services/jwtAuth')
 
 const app = express()
 const port = 5000
@@ -8,9 +10,14 @@ const port = 5000
 app.use(express.json())
 
 app.use('/api/ECAPP/signup', reg_router)
-app.use('/api/ECAPP/test', (req, res)=>{
-    console.log("test hit")
+app.use('/api/ECAPP/login', login_router)
+app.post('/api/ECAPP/test', jwtAuth, (req,res)=>{
+    return res.json({
+        'status': 'working',
+    })
 })
+
+
 app.listen(port, ()=>{
     console.log("sever started");
 })
